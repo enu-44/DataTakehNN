cbe033e5163bf89e961a81e57dd2c951c1da07c3
davidlambis <davidlambis261@gmail.com>
2017-11-15 12:11:59 -0500

    Fotos Activity

---
+++ app/src/main/java/com/datatakehnn/activities/fotos/FotosActivity.java
@@ -0,0 +1,15 @@
+package com.datatakehnn.activities.fotos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.datatakehnn.R;
+
+public class FotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fotos);
+    }
+}
