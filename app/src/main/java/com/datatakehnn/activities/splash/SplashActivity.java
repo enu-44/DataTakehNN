6ad0622af373b26ed2da1e0a878ddc976445611a
enu-44 <enuarmunozcastillo@gmail.com>
2017-11-16 18:36:44 -0500

    Arreglo splash android 4.4

--- app/src/main/java/com/datatakehnn/activities/splash/SplashActivity.java
+++ app/src/main/java/com/datatakehnn/activities/splash/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.datatakehnn.activities.splash;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
@@ -28,21 +29,32 @@ public class SplashActivity extends AppCompatActivity {
 
 
     public void loadAnimation() {
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
-        ivLogo.startAnimation(animation);
-        final Intent i = new Intent(this, LoginActivity.class);
-        Thread timer = new Thread() {
-            public void run() {
-                try {
-                    sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    startActivity(i);
-                    finish();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
+            ivLogo.startAnimation(animation);
+            final Intent i = new Intent(this, LoginActivity.class);
+            Thread timer = new Thread() {
+                public void run() {
+                    try {
+                        sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        startActivity(i);
+                        finish();
+                    }
                 }
-            }
-        };
-        timer.start();
+            };
+            timer.start();
+
+        }else{
+            Intent i = new Intent(this, SplashActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+
+
     }
 }
