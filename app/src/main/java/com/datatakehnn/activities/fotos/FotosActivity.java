57b5824982c30328cb6372d69239933c64ccfd51
enu-44 <enuarmunozcastillo@gmail.com>
2017-11-22 19:01:19 -0500

    Fotografias de novedades

--- app/src/main/java/com/datatakehnn/activities/fotos/FotosActivity.java
+++ app/src/main/java/com/datatakehnn/activities/fotos/FotosActivity.java
@@ -4,10 +4,15 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
@@ -18,15 +23,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.datatakehnn.R;
+import com.datatakehnn.activities.cables_elemento.adapter.AdapterCablesElemento;
+import com.datatakehnn.activities.fotos.adapter.OnItemClickListenerFoto;
+import com.datatakehnn.activities.fotos.adapter.RecyclerAdapterFoto;
 import com.datatakehnn.activities.menu.MainMenuActivity;
 import com.datatakehnn.controllers.ElementoController;
 import com.datatakehnn.controllers.FotoController;
+import com.datatakehnn.controllers.NovedadController;
 import com.datatakehnn.models.element_model.Elemento;
+import com.datatakehnn.models.elemento_cable.Elemento_Cable;
 import com.datatakehnn.models.foto_model.Foto;
+import com.datatakehnn.models.novedad_model.Novedad;
 import com.frosquivel.magicalcamera.MagicalCamera;
 import com.frosquivel.magicalcamera.MagicalPermissions;
 import com.raizlabs.android.dbflow.data.Blob;
@@ -36,7 +49,9 @@ import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
@@ -44,7 +59,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import id.zelory.compressor.Compressor;
 
-public class FotosActivity extends AppCompatActivity {
+public class FotosActivity extends AppCompatActivity implements OnItemClickListenerFoto, FotosMainView {
 
     //UI Elemenets
     @BindView(R.id.container)
@@ -60,6 +75,14 @@ public class FotosActivity extends AppCompatActivity {
     @BindView(R.id.edtDescripcionFoto2)
     EditText edtDescripcionFoto2;
 
+
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.txtResults)
+    TextView txtResults;
+
     //PERMISSION CAMARA
     private MagicalPermissions magicalPermissions;
     private int RESIZE_PHOTO_PIXELS_PERCENTAGE = 50;
@@ -69,6 +92,7 @@ public class FotosActivity extends AppCompatActivity {
     long elemento_id;
     boolean tomarFoto1 = false;
     boolean tomarFoto2 = false;
+    boolean tomarFotoNovedad = false;
     boolean yaTomoFoto1 = false;
     boolean yaTomoFoto2 = false;
     String ruta, path;
@@ -80,6 +104,23 @@ public class FotosActivity extends AppCompatActivity {
     //Instancias
     ElementoController elementoController;
     FotoController fotoController;
+    NovedadController novedadController;
+
+
+
+    //Dataset
+    List<Novedad> novedades;
+    Novedad novedad = new Novedad();
+    public  String Nombre_Novedad;
+
+
+    //contador de fotos
+    int contador = 0;
+
+    //Adapter
+    RecyclerAdapterFoto adapter;
+
+
 
 
     @Override
@@ -87,11 +128,13 @@ public class FotosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fotos);
         ButterKnife.bind(this);
+
         checkPermissionCamera();
         setToolbarInjection();
         setupInjection();
-        getCoordenadas();
-        getData();
+        initAdapter();
+        initRecyclerView();
+        loadNovedades();
     }
 
     //region SETUP INJECTION
@@ -102,11 +145,11 @@ public class FotosActivity extends AppCompatActivity {
     }
 
     private void setupInjection() {
-        this.elementoController = elementoController.getInstance(this);
-        this.fotoController = fotoController.getInstance(this);
-    }
-
-    private void getCoordenadas() {
+        this.elementoController = ElementoController.getInstance(this);
+        this.fotoController = FotoController.getInstance(this);
+        this.novedadController= NovedadController.getInstance(this);
+        elemento_id = elementoController.getLast().getElemento_Id();
+        novedades = novedadController.getListNovedadesByElementoId(elemento_id);
         Elemento elemento = elementoController.getLast();
         Latitud = elemento.getLatitud();
         Longitud = elemento.getLongitud();
@@ -114,9 +157,6 @@ public class FotosActivity extends AppCompatActivity {
         longitud = String.valueOf(Longitud);
     }
 
-    private void getData() {
-        elemento_id = elementoController.getLast().getElemento_Id();
-    }
     //endregion
 
     //region PERMISSION
@@ -155,6 +195,7 @@ public class FotosActivity extends AppCompatActivity {
             case R.id.ivFoto1:
                 tomarFoto1 = true;
                 tomarFoto2 = false;
+                tomarFotoNovedad = false;
                 if (TextUtils.isEmpty(edtDescripcionFoto1.getText().toString())) {
                     edtDescripcionFoto1.setError(getString(R.string.error_field_required));
                     focusView_ = edtDescripcionFoto1;
@@ -166,6 +207,7 @@ public class FotosActivity extends AppCompatActivity {
             case R.id.ivFoto2:
                 tomarFoto2 = true;
                 tomarFoto1 = false;
+                tomarFotoNovedad = false;
                 if (TextUtils.isEmpty(edtDescripcionFoto2.getText().toString())) {
                     edtDescripcionFoto2.setError(getString(R.string.error_field_required));
                     focusView_ = edtDescripcionFoto2;
@@ -214,11 +256,14 @@ public class FotosActivity extends AppCompatActivity {
                 //if you need save your bitmap in device use this method and return the path if you need this
                 //You need to send, the bitmap picture, the photo name, the directory name, the picture type, and autoincrement photo name if           //you need this send true, else you have the posibility or realize your standard name for your pictures.
                 ruta = magicalCamera.savePhotoInMemoryDevice(magicalCamera.getPhoto(), "IMG_POSTE_" + "Lat:" + latitud + "_Lng:" + longitud + "_foto2", "DataTakeCamara", MagicalCamera.JPEG, false);
+            }else if(tomarFotoNovedad== true){
+                ruta = magicalCamera.savePhotoInMemoryDevice(magicalCamera.getPhoto(), "IMG_POSTE_" + "Lat:" + latitud + "_Lng:" + longitud + Nombre_Novedad, "DataTakeCamara", MagicalCamera.JPEG, false);
             }
 
 
-            File file = saveBitmap(bitmap, ruta);
 
+
+            File file = saveBitmap(bitmap, ruta);
 
             try {
                 compressedImage = new Compressor(this)
@@ -237,7 +282,13 @@ public class FotosActivity extends AppCompatActivity {
             } else if (tomarFoto2 == true) {
                 path = magicalCamera.savePhotoInMemoryDevice(compressedImage, "IMG_POSTE_" + "Lat:" + latitud + "_Lng:" + longitud + "_foto2", "DataTakeCamara", MagicalCamera.JPEG, false);
                 registerFoto2();
+            }else if(tomarFotoNovedad== true){
+
+                path = magicalCamera.savePhotoInMemoryDevice(compressedImage, "IMG_POSTE_" + "Lat:" + latitud + "_Lng:" + longitud + Nombre_Novedad, "DataTakeCamara", MagicalCamera.JPEG, false);
+                updateFotoNovedad();
             }
+
+
 
 
             if (path != null) {
@@ -250,6 +301,14 @@ public class FotosActivity extends AppCompatActivity {
             Snackbar.make(container, "No pudo tomar la foto", Snackbar.LENGTH_SHORT).show();
         }
 
+    }
+
+    private void updateFotoNovedad() {
+        image = convertBitmapToByte(compressedImage);
+        Blob imagenBlob = new Blob(image);
+        novedad.setImage_Novedad(imagenBlob);
+        novedadController.updateWithFoto(novedad);
+        loadNovedades();
     }
 
     private void registerFoto1() {
@@ -298,8 +357,9 @@ public class FotosActivity extends AppCompatActivity {
         }
     }
 
-    //Convertir bitmap to File
 
+
+    //Convertir bitmap to File
     private File saveBitmap(Bitmap bitmap, String path) {
         File file = null;
         if (bitmap != null) {
@@ -313,7 +373,9 @@ public class FotosActivity extends AppCompatActivity {
             Snackbar.make(container, "Debe tomar Foto 1", Snackbar.LENGTH_SHORT).show();
         } else if (yaTomoFoto2 == false) {
             Snackbar.make(container, "Debe tomar Foto 2", Snackbar.LENGTH_SHORT).show();
-        } else {
+        } else if (contador < novedades.size()) {
+            Snackbar.make(container, "Debe registrar todas las fotos de novedades", Snackbar.LENGTH_SHORT).show();
+        }else {
             actualizarPoste();
         }
     }
@@ -369,8 +431,30 @@ public class FotosActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
-    //endregion
 
+
+    private void initAdapter() {
+        if (adapter == null) {
+            adapter = new RecyclerAdapterFoto(this, new ArrayList<Novedad>(), this);
+        }
+    }
+
+    private void initRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    private void loadNovedades() {
+        adapter.clear();
+        novedades.clear();
+        novedades = novedadController.getListNovedadesByElementoId(elemento_id);
+        setContent(novedades);
+        resultsList(novedades);
+        hideProgress();
+    }
+
+    //endregion
 
     //region MENU
     /*-------------------------------------------------------------------------------------------------------*/
@@ -390,7 +474,73 @@ public class FotosActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
     //endregion
 
+    //region IMPLEMENTS ONITEM CLICK LISTENER
+    @Override
+    public void onItemClick(Novedad item) {
+        tomarFoto2 = false;
+        tomarFoto1 = false;
+        tomarFotoNovedad = true;
+        Nombre_Novedad= item.getDetalle_Tipo_Novedad_Nombre();
+        novedad= item;
+        if (item.getImage_Novedad() == null) {
+            contador = contador + 1;
+        }
+
+        iniciarCamara();
+    }
+
+
+    //endregion
+
+    //region IMPLEMENTS METHODS FotosMainView
+    @Override
+    public void showProgresss() {
+        progressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onMessageOk(int colorPrimary, String message) {
+        int color = Color.WHITE;
+        Snackbar snackbar = Snackbar
+                .make(findViewById(R.id.container), message, Snackbar.LENGTH_LONG);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(ContextCompat.getColor(this, colorPrimary));
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_done, 0, 0, 0);
+        // textView.setCompoundDrawablePadding(getResources().getDimensionPixelOffset(R.dimen.activity_horizontal_margin));
+        textView.setTextColor(color);
+        snackbar.show();
+    }
+
+    @Override
+    public void onMessageError(int colorPrimary, String message) {
+        onMessageOk(colorPrimary, message);
+    }
+
+    @Override
+    public void resultsList(List<Novedad> listResult) {
+        String results = String.format(getString(R.string.results_global_search),
+                listResult.size());
+        txtResults.setText(results);
+    }
+
+    @Override
+    public void setContent(List<Novedad> items) {
+        adapter.setItems(items);
+    }
+
+
+    //endregion
 
 }
