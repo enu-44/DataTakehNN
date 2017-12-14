68d61c6bda3dc3f91c549cb50e8026c13f5873b7
davidlambis <davidlambis261@gmail.com>
2017-12-14 17:38:51 -0500

    Ajuste Permisos

--- app/src/main/java/com/datatakehnn/activities/login/LoginActivity.java
+++ app/src/main/java/com/datatakehnn/activities/login/LoginActivity.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -13,6 +16,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -78,7 +82,10 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
     private boolean Estado;
 
     //Permission
-    public static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
+    //public static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -168,7 +175,7 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
                     loginSqlite(edtUsuario.getText().toString(), edtContrasena.getText().toString());
                 } else {
 
-                    Request_Login request_login=new Request_Login(
+                    Request_Login request_login = new Request_Login(
                             usuario,
                             password,
                             Imei,
@@ -182,7 +189,7 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
                             Direccion_Ip,
                             Estado
                     );
-                    loginApiService.postLoginAsync(this,request_login);
+                    loginApiService.postLoginAsync(this, request_login);
                 }
                 //loginSqlite(edtUsuario.getText().toString(), edtContrasena.getText().toString());
             } else {
@@ -237,8 +244,8 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
     private void sendMenu(Usuario usuario) {
 
         Intent i = new Intent(this, SyncActivity.class);
-        i.putExtra("FROM_LOGIN",true);
-        i.putExtra("FROM_MENU",false);
+        i.putExtra("FROM_LOGIN", true);
+        i.putExtra("FROM_MENU", false);
         startActivity(i);
     }
 
@@ -256,11 +263,109 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
 
     @OnClick(R.id.fabLogin)
     public void onViewClicked() {
+        edtUsuario.setError(null);
+        edtContrasena.setError(null);
+        //mPasswordView.setError(null);
+        // Store values at the time of the login attempt.
+        String usuario = edtUsuario.getText().toString();
+        String password = edtContrasena.getText().toString();
+        ///String password = mPasswordView.getText().toString();
+        boolean cancel = false;
+        View focusView = null;
 
-        checkPermission();
+
+        // Validacion de campos
+        if (TextUtils.isEmpty(usuario)) {
+            edtUsuario.setError(getString(R.string.error_field_required));
+            focusView = edtUsuario;
+            cancel = true;
+        } else if (TextUtils.isEmpty(password)) {
+            edtContrasena.setError(getString(R.string.error_field_required));
+            focusView = edtContrasena;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (!hasPermissions(getApplicationContext(), PERMISSIONS)) {
+                    requestPermission();
+                } else {
+                    doPermissionGrantedStuffs();
+                }
+            }
+        }
     }
 
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+
+        //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doPermissionGrantedStuffs();
+                } /* else if ((Build.VERSION.SDK_INT >= 23 && !shouldShowRequestPermissionRationale(permissions[0])) ||
+                        (Build.VERSION.SDK_INT >= 23 && !shouldShowRequestPermissionRationale(permissions[1]))) {
+                    //Toast.makeText(MainActivity.this, "Go to Settings and Grant the permission to use this feature.", Toast.LENGTH_SHORT).show();
+                    // User selected the Never Ask Again Option
+                    Intent i = new Intent();
+                    i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    i.addCategory(Intent.CATEGORY_DEFAULT);
+                    i.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                    getApplicationContext().startActivity(i);
+
+                } */else {
+                    Toast.makeText(LoginActivity.this,
+                            "Permiso denegado", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
+   /* private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(LoginActivity.this, Manifest.permission.READ_PHONE_STATE );
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+
+    } */
+
+
+    /*
     private void checkPermission() {
+
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.READ_PHONE_STATE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -278,16 +383,17 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
                 // app-defined int constant. The callback method gets the
                 // result of the request.
             }
-        }else{
+        } else {
             doPermissionGrantedStuffs();
         }
-    }
+    } */
 
+    /*
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE : {
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -295,17 +401,29 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
                     // contacts-related task you need to do.
                     //Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
                     doPermissionGrantedStuffs();
+                } else if (Build.VERSION.SDK_INT >= 23 && !shouldShowRequestPermissionRationale(permissions[0])) {
+                    //Toast.makeText(MainActivity.this, "Go to Settings and Grant the permission to use this feature.", Toast.LENGTH_SHORT).show();
+                    // User selected the Never Ask Again Option
+                    Intent i = new Intent();
+                    i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    i.addCategory(Intent.CATEGORY_DEFAULT);
+                    i.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                    getApplicationContext().startActivity(i);
+
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
-                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Permiso Denegado", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
             // other 'case' lines to check for other
             // permissions this app might request
         }
-    }
+    }*/
 
     public void doPermissionGrantedStuffs() {
         //Have an  object of TelephonyManager
@@ -314,10 +432,17 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
         /// String SIMSerialNumber=tm.getSimSerialNumber();
+        for (String permission : PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+
+        /*
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
-            return;
-        }
+
+        }*/
         String Device_Id = equipment_Identifier.getDeviceID(telephonyManager, LoginActivity.this);
         String AndroidId = equipment_Identifier.getDeviceUniqueID(LoginActivity.this);
 
@@ -325,31 +450,30 @@ public class LoginActivity extends AppCompatActivity implements ILogin, Connecti
 
         /*
         String SIMCountryISO=tm.getSimCountryIso();*/
-        String Remote_Ip_Address=equipment_Identifier.NetwordDetect();
-        String Local_Ip_Addrss=equipment_Identifier.getLocalIpAddress();
-        String Android_Ver=equipment_Identifier.getAndroidVersion();
-        String MacAdd=equipment_Identifier.getMacAddr();
-        String DeviceName=equipment_Identifier.getDeviceName();
+        String Remote_Ip_Address = equipment_Identifier.NetwordDetect();
+        String Local_Ip_Addrss = equipment_Identifier.getLocalIpAddress();
+        String Android_Ver = equipment_Identifier.getAndroidVersion();
+        String MacAdd = equipment_Identifier.getMacAddr();
+        String DeviceName = equipment_Identifier.getDeviceName();
         ////Insertar Datos Equipo Maestro (Equipment master)
 
-        Imei=telephonyManager.getDeviceId();
-        Phone_Type_Device=Device_Id;
-        Android_Id=AndroidId;
-        Software_Version=Software_V;
-        Local_Ip_Address=Local_Ip_Addrss;
-        Android_Version=Android_Ver;
-        MacAddr=MacAdd;
-        Device_Name=DeviceName;
-        Direccion_Ip=Remote_Ip_Address;
-        Estado=false;
+        Imei = telephonyManager.getDeviceId();
+        Phone_Type_Device = Device_Id;
+        Android_Id = AndroidId;
+        Software_Version = Software_V;
+        Local_Ip_Address = Local_Ip_Addrss;
+        Android_Version = Android_Ver;
+        MacAddr = MacAdd;
+        Device_Name = DeviceName;
+        Direccion_Ip = Remote_Ip_Address;
+        Estado = false;
 
-        if(Imei.equals("") || Imei==null){
+        if (Imei.equals("") || Imei == null) {
             Snackbar.make(container, "El dispositivo no cuenta con IMEI, Contacte a su proveedor", Snackbar.LENGTH_SHORT).show();
-        }else{
+        } else {
             attemptLogin();
         }
     }
-
 
 
     //endregion
